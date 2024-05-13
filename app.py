e345a7ee2703fc339cbaaa9dc7ccf95acace3824
Adam Yanai <adam.yanai@gmail.com>
2024-05-13 06:01:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 import cv2
 import numpy as np
 
 app = Flask(__name__)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
-def detect_faces():
+def detect_faces(filter=None):
     while True:
         ret, frame = cap.read()
         if not ret:
@@ -18,6 +18,12 @@ def detect_faces():
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
             cropped_face = frame[y:y+h, x:x+w]
+
+            if filter == 'invert':
+                cropped_face = cv2.bitwise_not(cropped_face)
+            elif filter == 'thermal':
+                cropped_face = cv2.applyColorMap(cropped_face, cv2.COLORMAP_HOT)
+
             canvas = np.zeros((frame.shape[0], frame.shape[1] + cropped_face.shape[1], 3), dtype=np.uint8)
             canvas[:frame.shape[0], :frame.shape[1]] = frame
             canvas[:cropped_face.shape[0], frame.shape[1]:] = cropped_face
@@ -34,6 +40,23 @@ def index():
 def video_feed():
     return Response(detect_faces(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/filter')
+def filter():
+    return render_template('filter.html')
+
+@app.route('/filtered_video_feed')
+def filtered_video_feed():
+    filter = request.args.get('filter')  # Get the filter parameter from the URL
+    return Response(detect_faces(filter), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/filtertwo')
+def filtertwo():
+    return render_template('filtertwo.html')
+
+@app.route('/filtered_video_feed_two')
+def filtered_video_feed_two():
+    filter = request.args.get('filter')  # Get the filter parameter from the URL
+    return Response(detect_faces(filter), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 if __name__ == '__main__':
     app.run(debug=True)
-
